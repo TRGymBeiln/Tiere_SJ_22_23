43a297b379c3d27488d8fd05cb05edc1e2c6eb49
Berrrryyyy <andreasherman669@gmail.com>
2023-05-09 09:00:23 +0200

    Gepard und Maulwurf hinzugefügt

---
+++ Gepard.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Gepard.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Gepard implements TIER
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Gepard
+     */
+    public Gepard()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public String machtGeraeusch()
+    {
+        // tragen Sie hier den Code ein
+        return "bramar";
+    }
+}
