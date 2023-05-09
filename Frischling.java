7771c3b49e2410c5caeaba8412541ed2d3176912
Berrrryyyy <andreasherman669@gmail.com>
2023-05-09 09:15:50 +0200

    Frischling

---
+++ Frischling.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Frischling.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Frischling implements TIER
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+     private String name;
+
+    /**
+     * Konstruktor für Objekte der Klasse Frischling
+     */
+    public Frischling()
+    {
+        // Instanzvariable initialisieren
+        name = "Frischling";
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public String machtGeraeusch()
+    {
+        // tragen Sie hier den Code ein
+        return "RRQuitschRRQuitsch";
+    }
+}
