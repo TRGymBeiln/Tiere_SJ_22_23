6f619a35c3896bbb9fc947b3c6190779680351e0
Berrrryyyy <andreasherman669@gmail.com>
2023-05-09 09:23:46 +0200

    Murmeltier

---
+++ Murmeltier.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Murmeltier.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Murmeltier implements TIER
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+     private String name;
+
+    /**
+     * Konstruktor für Objekte der Klasse Murmeltier
+     */
+    public Murmeltier()
+    {
+        // Instanzvariable initialisieren
+        name = "Murmeltier";
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public String machtGeraeusch()
+    {
+        // tragen Sie hier den Code ein
+        return "HiiiiiHiiiii";
+    }
+}
