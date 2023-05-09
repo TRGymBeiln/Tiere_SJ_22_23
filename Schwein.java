902f7f8e2094753558ea22ff03e7cd3840f3d57f
max <maximilian.edenhofer@t-online.de>
2023-05-09 09:13:47 +0200

    Wildsau, Schwein hinzugefügt

---
+++ Schwein.java
@@ -0,0 +1,31 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Flamingo.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Schwein implements TIER
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private String name;
+
+    /**
+     * Konstruktor für Objekte der Klasse Flamingo
+     */
+    public Schwein()
+    {
+        // Instanzvariable initialisieren
+        name = "Schwein";
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public String machtGeraeusch(){
+        return "oink! oink!";
+    }
+}
